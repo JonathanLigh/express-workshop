13bc05be485a59de94c481ad5132958fafc394f3
Jonathan Ligh <jonathankligh@gmail.com>
2018-01-08 01:46:57 -0500

    solution up for review

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var chalk = require('chalk');
 var server = require('http').createServer();
 var express = require('express');
+var jokes = require('./jokeHandler');
 var app = express();
 
 server.on('request', app);
@@ -10,7 +11,11 @@ var PORT = 8000;
 server.listen(PORT, function() {
         console.log(chalk.blue('server started on port', chalk.magenta(PORT)));
     });
-/*
-Server routing code here:
 
-*/
+app.use('/knockknock', function(req, res){
+  res.status(200).send(jokes.createKnockKnock());
+});
+
+app.use('/oneliner', function(req, res){
+  res.status(200).send(jokes.createOneLiner());
+});
