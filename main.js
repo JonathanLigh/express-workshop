bb1a718ae8ae41dc02a78df40958524d53d45dec
Jonathan Ligh <jonathankligh@gmail.com>
2018-01-08 02:31:12 -0500

    up for review

--- main.js
+++ main.js
@@ -3,19 +3,44 @@ var server = require('http').createServer();
 var express = require('express');
 var jokes = require('./jokeHandler');
 var app = express();
+var utils = require('./utils');
 
 server.on('request', app);
+
+app.get('/knockknock', function(req, res){
+  res.status(200).send(jokes.createKnockKnock());
+});
+
+app.get('/knockknock/inCaps', function(req, res){
+  res.status(200).send(utils.inCaps(jokes.createKnockKnock()));
+});
+
+app.get('/knockknock/inQuotes', function(req, res){
+  res.status(200).send(utils.inQuotes(jokes.createKnockKnock()));
+});
+
+app.get('/knockknock/xTimes/:num', function(req, res){
+  res.status(200).send(utils.xTimes(jokes.createKnockKnock, req.params.num));
+});
+
+app.get('/oneliner', function(req, res){
+  res.status(200).send(jokes.createOneLiner());
+});
+
+app.get('/oneliner/inCaps', function(req, res){
+  res.status(200).send(utils.inCaps(jokes.createOneLiner()));
+});
+
+app.get('/oneliner/inQuotes', function(req, res){
+  res.status(200).send(utils.inQuotes(jokes.createOneLiner()));
+});
+
+app.get('/oneliner/xTimes/:num', function(req, res){
+  res.status(200).send(utils.xTimes(jokes.createOneLiner, req.params.num));
+});
 
 var PORT = 8000;
 
 server.listen(PORT, function() {
         console.log(chalk.blue('server started on port', chalk.magenta(PORT)));
     });
-
-app.use('/knockknock', function(req, res){
-  res.status(200).send(jokes.createKnockKnock());
-});
-
-app.use('/oneliner', function(req, res){
-  res.status(200).send(jokes.createOneLiner());
-});
